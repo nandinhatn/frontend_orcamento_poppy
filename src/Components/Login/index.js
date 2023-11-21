27d74ed0b4b9909eb293953a09c0baa16e83dd22
Fernanda Nogueira <fernanda@poppymidia.com.br>
2023-11-21 17:28:04 -0300

    Set login token UseContext and set axios api

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,6 +1,7 @@
 import react, {useState, useEffect, useContext} from 'react';
 import {MyContext} from '../../MyContext'
 import {Container} from './style'
+import api from '../Data/dates'
 
 
 
@@ -9,8 +10,14 @@ const Login = ()=>{
         const [password, setPassword]= useState('');
         const [user,setUser] = useState('');
 
-    const getDadosLogin = (user, password)=>{
-
+    const getDadosLogin = (e)=>{
+        
+        api.post('/api/login',{login: user, password: password}).then((res)=> {
+           /*  res.data.auth ? setLogin(res.data.auth) */
+            if(res.data.auth){
+                setLogin(res.data.token)
+            }
+            console.log(res.data)})
     }
         
     return(
@@ -18,9 +25,15 @@ const Login = ()=>{
         <Container>
 
         <div>Login</div>
+     
+        
+
         <input placeholder='Usuario' value={user} onChange={(e)=> setUser(e.target.value)}></input>
-        <input value={password} placeholder='Senha' onChange={(e)=> setPassword(e.target.value)}></input>
-        <button>Logar</button>
+        <input value={password} type='password' placeholder='Senha' onChange={(e)=> setPassword(e.target.value)}></input>
+        {user.length>0 && password.length>0 ? <button onClick={()=>getDadosLogin()}>Logar</button> : ''}
+       
+       
+        
         </Container>
         </>
     )
